4420948f80abbe5c9c9c97d4557951d7a36f8643
Ivo Rojas <ivo753@gmail.com>
2021-11-22 09:05:05 -0400

    04 Added Sort by Option

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -17,17 +17,35 @@ interface ListProps {
 }
 
 const List: FC<ListProps> = ({ value }) => {
-  
   const [list, setList] = useState(value);
   const [filter, setFilter] = useState("ALL");
+  const [sort, setSort] = useState("");
 
   const handleFilterChange = (event: SelectChangeEvent<string>) => {
     setFilter(event.target.value);
     setList(
-      value.filter((device) => {
+      [...value].filter((device) => {
         if (event.target.value === "ALL") return true;
-        return device.type === event.target.value;
+        return device?.type === event.target.value;
       })
+    );
+  };
+
+  const handleSortbyChange = (event: SelectChangeEvent<string>) => {
+    setSort(event.target.value);
+    setList(
+      event.target.value === ""
+        ? [...list]
+        : [...list].sort((a, b) => {
+            if (event.target.value === "SystemName") {
+              return a.system_name.localeCompare(b.system_name);
+            }
+            if (event.target.value === "HDDCapacity") {
+              console.log(a.hdd_capacity, b.hdd_capacity);
+              return parseInt(a.hdd_capacity) - parseInt(b.hdd_capacity);
+            }
+            return 0;
+          })
     );
   };
 
@@ -35,12 +53,12 @@ const List: FC<ListProps> = ({ value }) => {
     <>
       <div>
         <FormControl sx={{ m: 1, minWidth: 140 }}>
-          <InputLabel id="device-type">Filter by device type</InputLabel>
+          <InputLabel id="device-type">Show by device type</InputLabel>
           <Select
             labelId="device-type"
             id="device-type-id"
             value={filter}
-            label="Filter by device type"
+            label="Show by device type"
             onChange={handleFilterChange}
           >
             <MenuItem value={"ALL"}>All</MenuItem>
@@ -49,6 +67,20 @@ const List: FC<ListProps> = ({ value }) => {
             </MenuItem>
             <MenuItem value={"WINDOWS_SERVER"}>Windows Server</MenuItem>
             <MenuItem value={"MAC"}>Mac</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl sx={{ m: 1, minWidth: 140 }}>
+          <InputLabel id="sort-by">Sort by</InputLabel>
+          <Select
+            labelId="sort-by"
+            id="sort-by-id"
+            value={sort}
+            label="Sort by"
+            onChange={handleSortbyChange}
+          >
+            <MenuItem value={""}>None</MenuItem>
+            <MenuItem value={"SystemName"}>System Name</MenuItem>
+            <MenuItem value={"HDDCapacity"}>HDD Capacity</MenuItem>
           </Select>
         </FormControl>
       </div>
